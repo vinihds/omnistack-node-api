72c75762f2deeccdcf1481550fa5cef42100d2d5
unknown <vinicius.silveira8@hotmail.com>
2019-08-18 20:41:05 -0300

    Finalização recursos avançados

--- src/server.js
+++ src/server.js
@@ -4,14 +4,43 @@ const cors = require('cors');
 
 const routes = require('./routes');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-yohvt.mongodb.net/omnistack?retryWrites=true&w=majority', {
+const connectedUsers = {};
+
+io.on('connection', socket => {
+  const { user } = socket.handshake.query;
+
+  connectedUsers[user] = socket.id;
+});
+
+// mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-yohvt.mongodb.net/omnistack?retryWrites=true&w=majority', {
+//   useNewUrlParser: true
+// });
+
+mongoose.connect('mongodb://localhost:27017/omnistack', {
   useNewUrlParser: true
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+mongoose.connection.on('error', function (error) {
+  console.error('Database connection error:', error);
+});
+
+mongoose.connection.once('open', function () {
+  console.log('Database connected');
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+})
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
